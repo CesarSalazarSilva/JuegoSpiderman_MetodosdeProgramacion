38827fcff4ef542d13507288f598aa550ca10721
cccarl <carlos.barahona.t@usach.cl>
2019-01-02 20:28:16 -0300

    Add files via upload

--- BES.c
+++ BES.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//POR MUY ORGULLOSO QUE ESTE DE ESTA FUNCION AL FINAL NO LA USARE
 /*Funcion recursiva que le suma 1 a la combinacion actual, al menos que se llegue a su limite, en tal caso
 se cambia a 0 y aplica la misma funcion a la sigiuente linea, recursivamente
 Entrada: arreglo de enteros con los limites de las combinaciones, arreglo de enteros con la combinacion actual,
@@ -22,52 +23,135 @@ void sigCombinacion(int * combFinal, int * combActual, int tamanio, int pos){
 	}
 }
 
-/*Funcion que recibe al archivo de la ciudad codificado para procesar el segmento de las filas,
-crea una matriz con los numeros y una lista de cada combinacion de cada fila
-Entrada: puntero a un archivo/puntero suma total de las combinaciones/tamanio de la ciudad/lista con cada combinaicon de cada fila
-Salida: matriz de enteros con las los mismos numeros de la parte de filas del archivo*/
-int ** procesarFilas(FILE * archivo, double * combTotales, int cantidad, int *listCombinaciones){
+/*Funcion que recibe al archivo de la ciudad codificado para procesar el segmento de las filas
+Entrada: puntero a un archivo/tamanio de la ciudad
+Salida: matriz con las filas mostradas en el archivo*/
+int ** procesarFilas(FILE * archivo, int tamanio){
 
 	int ** filas;
-	filas = (int**)malloc(sizeof(int*)*cantidad);
+	filas = (int**)malloc(sizeof(int*)*tamanio);
 
 	//aqui se guarda la parte de las filas como matriz de enteros
-	int i, j, avanzar, espaciosLibres, cantBloqEdif, cantEdifFila, combinaciones;
-	avanzar = 0;
-	*combTotales = 1;
+	int i, j, cantEdif, cantEdifFila;
+	for(i=0; i<tamanio; i++){
 
-	for(i=0; i<cantidad; i++){
-		//se guarda el primer numero de las filas para saber cuantos bloques de edificios hay
-		fscanf(archivo, "%d ", &cantBloqEdif);
-		printf ("Prueba cant edificios: <%d>\n", cantBloqEdif);
-		filas[i] = (int*)malloc(sizeof(int)*(cantBloqEdif+1));
-		filas[i][0] = cantBloqEdif;
+		//se guarda el primer numero de las filas para saber cuantos edificios hay
+		fscanf(archivo, "%d ", &cantEdif);
+		filas[i] = (int*)malloc(sizeof(int)*(tamanio+1));
+		filas[i][0] = cantEdif;
 
 		//creacion de la matriz de las filas
 		cantEdifFila = 0;
-		for(j=1; j<cantBloqEdif+1; j++){
+		for(j=1; j<cantEdif+1; j++){
 			 fscanf(archivo, "%d ", &filas[i][j]);
-			 printf("Prueba n edif: <%d>\n", filas[i][j]);
 			 cantEdifFila++;
 		}
-
-		//un cero de sobra para que se sepa cuando se termina de leer las filas
-		filas[i][cantidad] = 0;
-
-		//se calculan las combinaciones posibles de edificios por fila
-		espaciosLibres = cantidad - cantEdifFila;
-		//como siempre es 1 bloque de edificios seguidos, las posibles combinaciones son bastante simples:
-		combinaciones = espaciosLibres+1;
-		//editamos los valores de la lista de combinaciones (si, tambien cambian en las otras funciones)
-		listCombinaciones[i] = combinaciones;
-		//se calcula la suma total de combinaciones a hacer
-		*combTotales *= combinaciones;
-		printf("combinaciones de la fila: %d\n\n", listCombinaciones[i]);
 	}
-	printf("COMINACIONES TOTALES: %f\n", *combTotales);
+
+	int prueba;
+	//parte para probar filas, cambiar el numero para no imprmir
+	prueba = 0;
+	if (prueba == 0){
+		printf("--------------------\nPrueba Filas\n");
+		for(i=0; i<tamanio; i++){
+			printf("fila %d: ", i);
+			for(j=0; j<tamanio+1; j++){
+				printf("%d ", filas[i][j]);
+			}
+			printf("\n");
+		}
+		printf("--------------------\n");
+	}
 
 	return filas;
+}
 
+/*Funcion que procesa la segunda mitad del archivo, las partes de las columnas, 
+y separa los 2 datos que entrega en 2 matrices.
+Entrada: puntero a un archivo/tamanio de la ciudad/matriz de los numeros de cada edificio a rellenar/
+matriz que indica cuantos edificios seguidos hay, a rellenar
+Salida: void*/ 
+void procesarColumnas(FILE * archivo, int tamanio, int ** colNumeros, int ** colEdifSeguidos){
+
+	int i, j, k, aux, edifTotal;
+	edifTotal = 0;
+	char linea[tamanio*4+2];
+	for(i=0; i < tamanio; i++){
+		colNumeros[i] = (int*)malloc(sizeof(int)*tamanio);
+		colEdifSeguidos[i] = (int*)malloc(sizeof(int)*tamanio);
+
+		fgets(linea, tamanio*4+2, archivo);
+
+		//caso de columna vacia
+		if (linea[0] == '0'){
+			colNumeros [i][0] = 0;
+			colEdifSeguidos[i][0] = 0;
+		}
+		else{
+			for(j=0; j < tamanio*2; j+=2){
+				//caso en que solo haya 1 edificio en la columna
+				if(j==0 && linea[1] != '-'){
+					//truco para transformar un char del 0 al 9 en int
+					aux = linea[0] - '0';
+					colEdifSeguidos[i][0] = aux;
+					aux = linea[2] - '0';
+					colNumeros[i][0] = aux;
+				}
+				//caso para contar cuantos edificios seguidos hay
+				else if (linea[j+1] == '-' || (j>0 && linea[j-1] == '-')){
+					aux = linea[j] - '0';
+					colEdifSeguidos[i][j/2] = aux;
+					edifTotal += aux;
+
+					//aqui se procesa la parte de los numeros de los edificios, para hacerlo solo 1 vez por col
+					//primero se verifica que se este en el ultimo numero con guion
+					if ((j>0 && linea[j-1] == '-') && (linea[j+1] == ' ')){
+						//aqui se guardan los numeros de cada edificio
+						for (k=0; k<edifTotal; k++){
+							aux = linea[2*k+j+2] - '0';
+							colNumeros[i][k] = aux;
+						}
+						//aqui se rellena el resto con ceros
+						for (k = tamanio-1; k >= edifTotal; k--){
+							colNumeros[i][k] = 0;
+						}
+					}
+
+				}
+				//ahora se rellena el resto con ceros para dejar una matriz cuadrada sin vacios
+				else{
+					colEdifSeguidos[i][j/2] = 0;
+				}
+			}
+			edifTotal = 0;
+		}
+	}
+	//parte para probar colNumeros, cambiar el numero para no imprmir
+	int prueba;
+	prueba = 0;
+	if (prueba == 0){
+		printf("--------------------\nPrueba colNumeros\n");
+		for(i=0; i<tamanio; i++){
+			for(j=0; j<tamanio; j++){
+				printf("%d ", colNumeros[i][j]);
+			}
+			printf("\n");
+		}
+	printf("--------------------\n");
+	}
+	//parte para probar colEdifSeguidos , cambiar el numero para no imprmir
+	prueba = 0;
+	if (prueba == 0){
+		printf("--------------------\nPrueba colEdifSeguidos\n");
+		for(i=0; i<tamanio; i++){
+			for(j=0; j<tamanio; j++){
+				printf("%d ", colEdifSeguidos[i][j]);
+			}
+			printf("\n");
+		}
+	printf("--------------------\n");
+	}
+	return;
 }
 
 //funcion que genera la matriz de la ciudad a partir de un archivo (usando busqueda en espacio de soluciones)
@@ -92,69 +176,32 @@ int ** generarCiudad(char * nombreArchivo, int *tamanio){
 	printf("tamanio de ciudad: %dx%d\n", *tamanio, *tamanio);
 
 
-	//se creara el espacio de soluciones aqui
+	//****se creara el espacio de soluciones aqui****
 	//procesamiento de las filas
 	int ** filas;
-	double combTotales;
-	int * listCombinaciones = (int*)malloc(sizeof(int)*(*tamanio));
-	filas = procesarFilas(archPuntero, &combTotales, *tamanio, listCombinaciones);
+	filas = procesarFilas(archPuntero, *tamanio);
 
-	//lista que parte con ceros y representa cada combinacion de cada ciudad
-	int * listCombinacionActual = (int*)malloc(sizeof(int)*(*tamanio));
-	double i;
-	for(i = 0; i<*tamanio; i++)	listCombinacionActual[i] = 0;
+	//procesamiento de las columnas
+	int ** colNumeros;
+	int ** colEdifSeguidos;
+	colNumeros = (int**)malloc(sizeof(int*)*(*tamanio));
+	colEdifSeguidos = (int**)malloc(sizeof(int*)*(*tamanio));
+	procesarColumnas(archPuntero, *tamanio, colNumeros, colEdifSeguidos);
+	fclose(archPuntero);
 
 	//creacion de tadas las ciudades posibles
-	int *** ciudadesPosibles = (int***)malloc(sizeof(int**)*combTotales);
-	int j, k, escrituraFilas, posFilas;
+	int *** ciudadesPosibles = (int***)malloc(sizeof(int**)*1000); 
+	//RECORDAR: ver si se puede calcular esto en vez de dejar un numero al ojo
 
-
-	//variable con la posicion a leer de la matriz de filas
-	posFilas = 1;
-	//variable que dira que escribir en la fila de las ciudades
-	//0: escribir ceros, 1: escribir los edificios, 2: escribir ceros
-	escrituraFilas = 0;
-	//ciudades
-	for(i = 0; i<combTotales; i++){
-		//printf("ciudad numero %d\n", i);
-		ciudadesPosibles[i] = (int**)malloc(sizeof(int*)*(*tamanio));
-		printf("TAATATAT\n");
-		//1 ciudad
-		for(j = 0; j < *tamanio; j++){
-			ciudadesPosibles[i][j] = (int*)malloc(sizeof(int)*(*tamanio));
-			//1 fila de 1 ciudad
-			for (k = 0; k < *tamanio; k++){
-
-				if (k == listCombinacionActual[j])
-					escrituraFilas = 1;
-
-				if (escrituraFilas == 0) {
-					ciudadesPosibles[i][j][k] = 0;
-				}
-
-				if (escrituraFilas == 1){
-					if (filas[j][posFilas] == 0)
-						escrituraFilas = 2;
-					else{
-						ciudadesPosibles[i][j][k] = filas[j][posFilas];
-						posFilas++;
-					}
-				}
-
-				if (escrituraFilas == 2)
-					ciudadesPosibles[i][j][k] = 0;
-				
-				
-				printf("%d ", ciudadesPosibles[i][j][k]);
-			}
-			escrituraFilas = 0;
-			posFilas = 1;
-			printf("\n");
-		}
-		//esta funcion hace que se pase a la siguiente combinatoria
-		sigCombinacion(listCombinaciones, listCombinacionActual, *tamanio, 0);
-		printf("\n");
+	int i, j;
+		/*
+	for (i = 0; i<*tamanio; i++){
+		//creacion de una ciudad posible
+		int ** ciudadesPosibles = (int**)malloc(sizeof(int*)*(*tamanio)); 
+		for(j = 0; j< )
+		if (colNumeros[i])
 	}
+*/
 
 
 	//salida temporal para que corra el programa, una matriz 5x5 rellena con 0-24 en orden
@@ -177,12 +224,6 @@ int ** generarCiudad(char * nombreArchivo, int *tamanio){
 	for(i=0; i<*tamanio; i++){
 		free(filas[i]);
 	}
-	for (i=0; i<1000; i++){
-		for (j = 0; j < *tamanio; ++j){
-			free(ciudadesPosibles[i][j]);
-		}
-		free(ciudadesPosibles[i]);
-	}
 	free(ciudadesPosibles);
 	free(filas);
 	fclose(archPuntero);
@@ -194,7 +235,7 @@ int main(){
 	int ** ciudad;
 	int tamanio;
 	ciudad = generarCiudad("entrada.txt", &tamanio);
-	printf("COSA en main: <%d>\n", ciudad[0][0]);
+	printf("ciudad [0][0] en main: <%d>\n", ciudad[0][0]);
 	
 	//liberacion de la memoria usada por la matriz
 	int i;
